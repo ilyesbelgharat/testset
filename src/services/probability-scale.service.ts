959d9765bca64d6ae06f16caf86807a9396cac04
ilyesbelgharat <53319470+ilyesbelgharat@users.noreply.github.com>
2019-07-26 19:45:03 -0400

    Update probability-scale.service.ts

--- src/services/probability-scale.service.ts
+++ src/services/probability-scale.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Http} from '@angular/http';
 import {map} from 'rxjs/operators';
-import {ProbabilityScale} from '../model/model.ProbabilityScale';
+import {ProbabilityScale} from '../model/model.probabilityScale';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {AuthentificationService} from './authentification.service';
 
