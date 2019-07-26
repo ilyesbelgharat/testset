110ff43e870bbd9ec918f5f9d7869e9230b2cf18
ilyesbelgharat <53319470+ilyesbelgharat@users.noreply.github.com>
2019-07-26 19:43:36 -0400

    Update source-danger.service.ts

--- src/services/source-danger.service.ts
+++ src/services/source-danger.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Http} from '@angular/http';
 import {map} from 'rxjs/operators';
-import {SourceDanger} from '../model/model.SourceDanger';
+import {SourceDanger} from '../model/model.sourceDanger';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {AuthentificationService} from './authentification.service';
 @Injectable({
