d7e0c45f5ebe4a368254f81f4eafe3cd1cc7ea78
ilyesbelgharat <53319470+ilyesbelgharat@users.noreply.github.com>
2019-07-26 20:05:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/@coreui/coreui-free-angular-admin-template'));
 
 app.all('*', (req, res) => {  
-  res.status(200).sendFile(__dirname + '/dist/index.html');  
+  res.status(200).sendFile(__dirname + '/dist/@coreui/coreui-free-angular-admin-template/index.html');  
 });  
 app.listen(process.env.PORT || 8080);  
 
